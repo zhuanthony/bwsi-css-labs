cb28f1caa4e8ccb0a2d67e9ce4c5eca015bae176
zhuanthony <anthonyzhu16@gmail.com>
2026-02-16 01:55:51 -0500

    add unit tests for simple_calculator

--- tests/tests_1b.py
+++ tests/tests_1b.py
@@ -8,9 +8,10 @@ import pytest
 from labs.lab_1.lab_1b import simple_calculator
 
 def test_addition():
-    assert simple_calculator("add", 5, 3) == 8          # Test for positive numbers
+    assert simple_calculator("add", 5, 3) == 10          # Test for positive numbers
     assert simple_calculator("add", -2, 2) == 0         # Test for negative and positive number
     assert simple_calculator("add", 0, 0) == 0          # Test for zero addition
+    assert simple_calculator("add", 60, 7) == 76 
 
 def test_subtraction():
     assert simple_calculator("subtract", 5, 3) == 2     # Test for positive numbers
